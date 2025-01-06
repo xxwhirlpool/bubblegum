c1fa8e37349a777c77720e4731bb6f0cb4850683
xxwhirlpool <melodicake@disroot.org>
2025-01-06 17:34:17 -0500

    add red carpet for optional markdown and some css fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+def markdown(text)
+    options = [:hard_wrap, :autolink, :no_intra_emphasis, :fenced_code_blocks, :underline,
+    :highlight, :safe_links_only, :prettify, :no_styles]
+    Markdown.new(text, *options).to_html.html_safe
+  end
+
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,4 @@
 
 pin "application"
 pin "asciinema-player" # @3.8.1
+pin "@github/markdown-toolbar-element", to: "https://ga.jspm.io/npm:@github/markdown-toolbar-element@2.1.1/dist/index.js"
