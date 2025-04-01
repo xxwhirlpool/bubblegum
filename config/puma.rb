31216b39d52898f334e5af7f11bae72b04f37b12
xxwhirlpool <melodicake@disroot.org>
2025-04-01 19:03:24 -0400

    (hopefully) fixes incorrect RSS feed formatting. also some deployment changes that didn't work and gemfile updates

--- config/puma.rb
+++ config/puma.rb
@@ -23,10 +23,10 @@ end
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+port ENV.fetch("PORT") { 3003 }
 
 # Specifies the `environment` that Puma will run in.
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
