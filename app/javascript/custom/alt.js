91b46bba6852aa455bf69077f37880e2e33dfbd2
xxwhirlpool <witnesslachesis@disroot.org>
2026-03-05 18:04:27 -0500

    misc

--- app/javascript/custom/alt.js
+++ app/javascript/custom/alt.js
@@ -1,12 +1,14 @@
-const allImgs = document.querySelectorAll("img[alt]");
+const allImgs = document.querySelectorAll("article img[alt]");
 
 function altButton() {
 	allImgs.forEach((im) => {
 		const det = document.createElement("details");
 		const sum = document.createElement("summary");
+		det.classList.add("alt-det");
+		sum.classList.add("alt-sum");
 		sum.textContent = "alt text";
-		det.appendChild(sum);
 		det.textContent = im.getAttribute("alt");
+		det.appendChild(sum);
 		im.insertAdjacentElement("afterend", det);
 	})
 }
